2a65b25934b708b98db8d02670d9370f95892e09
SageCreations <sagecreations.ec@gmail.com>
2022-08-18 11:26:42 -0500

    added a sprite for lvl2, edited the load level script for the main menu

--- DungeonBosses/Scripts/Level_1_Scripts/start_menu.gd
+++ DungeonBosses/Scripts/Level_1_Scripts/start_menu.gd
@@ -1,31 +1,36 @@
 extends Control
 
+# make variables for levels here
+export(PackedScene) var level_one
+export(PackedScene) var boss_level
+export(PackedScene) var controls_level
+export(PackedScene) var credits_level
 
-
+# function to load scene called
 func button_pressed(scene):
-	#get_tree().change_scene_to(scene)
-	get_tree().change_scene(scene)
+	get_tree().change_scene_to(scene)
+	#get_tree().change_scene(scene)
 
-
+ # function to quit window
 func quit():
 	get_tree().quit()
 
-
+# start button
 func _on_start_button_pressed():
-	button_pressed("res://_LevelScenes/Level_1.tscn")
+	button_pressed(level_one)
 
-
+# credits button
 func _on_credits_button_pressed():
-	button_pressed("res://_LevelScenes/credits.tscn")
+	button_pressed(credits_level)
 
-
+# quit button
 func _on_quit_button_pressed():
 	quit()
 
-
+# controls button
 func _on_controls_button_pressed():
-	button_pressed("res://_LevelScenes/controls.tscn")
+	button_pressed(controls_level)
 
-
+# boss level button
 func _on_boss_level_pressed():
-	button_pressed("res://_LevelScenes/boss_level.tscn")
+	button_pressed(boss_level)
