ea2a37cb163b98a34fb69ba299763ba9074ae88d
SageCreations <sagecreations.ec@gmail.com>
2022-08-18 13:47:33 -0500

    started working hub_world
    read TODO on the load_next_scene script

--- DungeonBosses/Prefabs/Level_1_Prefabs/Terrain/load_next_scene.gd
+++ DungeonBosses/Prefabs/Level_1_Prefabs/Terrain/load_next_scene.gd
@@ -2,6 +2,16 @@ extends Node2D
 
 export(PackedScene) var next_level
 
+export(bool) var input_needed = false
+
 func _on_Area2D_body_entered(body):
 	if (body.get_name() == "Player"):
-		get_tree().change_scene_to(next_level)
+		print_debug("player has entered 'load next area' box")
+		if (input_needed == true):
+			print_debug("input is needed!")
+			if(Input.is_action_just_pressed("hub_world_go_into")):
+				print_debug("Correct button was pressed!!!")
+				# TODO: this part isnt executing, maybe need a type of listener function (research)
+				get_tree().change_scene_to(next_level)
+		else:
+			get_tree().change_scene_to(next_level)
